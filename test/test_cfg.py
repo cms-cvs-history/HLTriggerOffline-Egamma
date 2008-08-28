14a80f525e272821d500efee745804aad36b9459
Matthias Ulrich Mozer <sha1-03a50c2c36d55df29636c6c60b13205b37358346@cern.ch>
2008-08-28 08:07:52 +0000

    moved preselection to _cff.py file

--- test/test_cfg.py
+++ test/test_cfg.py
@@ -1,20 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("dqm")
-process.load("HLTriggerOffline.Egamma.doubleElectronDQM_cfi")
-process.load("HLTriggerOffline.Egamma.doubleElectronRelaxedDQM_cfi")
-process.load("HLTriggerOffline.Egamma.doublePhotonDQM_cfi")
-process.load("HLTriggerOffline.Egamma.doublePhotonRelaxedDQM_cfi")
-process.load("HLTriggerOffline.Egamma.highEtDQM_cfi")
-process.load("HLTriggerOffline.Egamma.singleElectronDQM_cfi")
-process.load("HLTriggerOffline.Egamma.singleElectronLargeWindowDQM_cfi")
-process.load("HLTriggerOffline.Egamma.singleElectronRelaxedDQM_cfi")
-process.load("HLTriggerOffline.Egamma.singleElectronRelaxedLargeWindowDQM_cfi")
-process.load("HLTriggerOffline.Egamma.singlePhotonDQM_cfi")
-process.load("HLTriggerOffline.Egamma.singlePhotonRelaxedDQM_cfi")
-process.load("HLTriggerOffline.Egamma.veryHighEtDQM_cfi")
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
-process.load("PhysicsTools.HepMCCandAlgos.genParticleCandidates_cfi")
+
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
@@ -36,38 +23,15 @@ process.source = cms.Source("PoolSource",
         '/store/relval/CMSSW_2_1_4/RelValWE/GEN-SIM-DIGI-RAW-HLTDEBUG/STARTUP_V5_v1/0005/B25CA5CA-8A6C-DD11-8884-000423D6B358.root')
 )
 
-process.leptons = cms.EDFilter("PdgIdAndStatusCandViewSelector",
-    status = cms.vint32(1),
-    src = cms.InputTag("genParticles"),
-    pdgId = cms.vint32(11)
-)
-
-process.cut = cms.EDFilter("EtaPtMinCandViewSelector",
-    src = cms.InputTag("leptons"),
-    etaMin = cms.double(-2.5),
-    etaMax = cms.double(2.5),
-    ptMin = cms.double(2.0)
-)
-
-process.sel = cms.EDFilter("CandViewCountFilter",
-    src = cms.InputTag("cut"),
-    minNumber = cms.uint32(1)
-)
-
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string('dqmtest.root')
-)
-
-process.test = cms.Path(process.leptons*process.cut*process.sel*process.doubleElectronDQM+process.doubleElectronRelaxedDQM+process.doublePhotonDQM+process.doublePhotonRelaxedDQM+process.highEtDQM+process.veryHighEtDQM+process.singleElectronDQM+process.singleElectronLargeWindowDQM+process.singleElectronRelaxedDQM+process.singleElectronRelaxedLargeWindowDQM+process.singlePhotonRelaxedDQM+process.singlePhotonDQM)
-process.highEtDQM.pdgGen = 11
-process.veryHighEtDQM.pdgGen = 11
+process.load("HLTriggerOffline.Egamma.EgammaValidation_Wenu_cff")
+process.test = cms.Path(process.egammavalWenu)
 
 process.load("DQMServices.Core.DQM_cfg")
 
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
-
 process.p = cms.EndPath(process.dqmSaver)
+
 process.DQMStore.verbose = 0
 process.DQM.collectorHost = ''
 process.dqmSaver.convention = 'Online'
